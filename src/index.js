3a5abbfb465455a732f9ba5d934b7c66507b7ce1
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-04-27 13:26:36 -0700

    capitalize

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// challenge 1: capitalize, makes the first letter of a given string uppercase
+function capitalize(str){
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+console.log(capitalize('hello world'))
+
+//challenge 2: allCaps, makes allk characters uppercase
+
+//challenge 3: capitalizeWords, makes the first character of each word uppercase
+
+//challenge 4: removeExtraSpace, removes all spaces
+
+//challenge 5: kebobCase, removes spaces and replaces them with hyphens
+
+//challenge 6: snakeCase, removes spaces and replaces them with underscores
+
+//challenge 7: camelCase, lowercases the first word, removes spaces, then uppercases the other words
+
+//challenge 8: shift, take the first character and move it to the end
+
+//challenge 9: makeHashTag, make the word to camelCase and then put a # at the beginning
